b2bf5a5d46a7f9efbfe245f82782dc8e709bc91c
Frank Schüler <schuelerfrank@gmx.de>
2022-01-26 15:22:48 +0100

    fix for npm package definition

--- browser.js
+++ browser.js
@@ -2,8 +2,7 @@ import GLOBAL from "@default-js/defaultjs-common-utils/src/Global";
 import {Requester} from "./index"
 
 GLOBAL.defaultjs = GLOBAL.defaultjs || {};
-GLOBAL.defaultjs.dynamic = GLOBAL.defaultjs.dynamic || {};
-if(!GLOBAL.defaultjs.dynamic.Requester){
-	GLOBAL.defaultjs.dynamic.Requester = Requester;
+if(!GLOBAL.defaultjs.DynamicRequester){
+	GLOBAL.defaultjs.DynamicRequester = Requester;
 	Requester.VERSION = "${version}";
 }
